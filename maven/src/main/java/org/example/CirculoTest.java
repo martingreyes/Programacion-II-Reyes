bd08bb0d740c187f69608308ca9a262fc04c53ab
Martin Reyes <martinreyes@macOS.local>
2023-05-29 16:08:47 -0300

    Ejemplo de Maven y Junit

--- maven/src/main/java/org/example/CirculoTest.java
+++ maven/src/main/java/org/example/CirculoTest.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,42 +15,33 @@ public class CirculoTest {
     public void preparacion() {
         this.circulo1 = new Circulo("circulo", 10);
         this.circulo2 = new Circulo("otro circulo", 14);
-        System.out.println("JAJAJAJ");
     }
 
     @Test
     public void testCalcularPerimetro() {
-        this.circulo1 = new Circulo("circulo", 10);
-        this.circulo2 = new Circulo("otro circulo", 14);
         assertEquals(62.83185307179586, this.circulo1.calcularPerimetro(), 0.0001);
         assertEquals(87.96459430051421, this.circulo2.calcularPerimetro(), 0.0001);
     }
 
     @Test
     public void testCalcularSuperficie() {
-        this.circulo1 = new Circulo("circulo", 10);
-        this.circulo2 = new Circulo("otro circulo", 14);
         assertEquals(314.1592653589793, this.circulo1.calcularSuperficie(), 0.0001);
         assertEquals(615.7521601035994, this.circulo2.calcularSuperficie(), 0.0001);
     }
 
     @Test
     public void testToString() {
-        this.circulo1 = new Circulo("circulo", 10);
-        this.circulo2 = new Circulo("otro circulo", 14);
         assertEquals("Circulo(nombre=circulo, radio=10.0)", this.circulo1.toString());
         assertEquals("Circulo(nombre=otro circulo, radio=14.0)", this.circulo2.toString());
     }
 
-//    @Test(expected = EjemploException.class)
-//    public void testExcepcion1() throws EjemploException{
-//        this.circulo1.excepcionSiCero(0);
-//    }
+    @Test(expected = EjemploException.class)
+    public void testExcepcion1() throws EjemploException{
+        this.circulo1.excepcionSiCero(0);
+    }
 
     @Test
     public void testExcepcion2() {
-        this.circulo1 = new Circulo("circulo", 10);
-        this.circulo2 = new Circulo("otro circulo", 14);
         try {
             this.circulo1.excepcionSiCero(0);
         } catch (EjemploException e) {
